147f82a77272fb5991fe16097333c2a031722611
Julie <ju.ralph@gmail.com>
2013-12-26 18:15:22 -0800

    Deleting unused files.

--- spec/sample_spec.js
+++ spec/sample_spec.js
@@ -6,8 +6,6 @@ describe('mini-jasmine-node', function() {
   it('shows asynchronous test node-style', function(done) {
     setTimeout(function() {
       expect('second').toEqual('second');
-      // If you call done() with an argument, it will fail the spec 
-      // so you can use it as a handler for many async node calls
       done();
     }, 1);
     expect('first').toEqual('first');
--- specs.sh
+++ specs.sh
@@ -6,12 +6,12 @@ echo "All these tests should pass"
 command="${entry} spec/*_spec.js"
 echo $command
 time $command #/nested/uber-nested
-echo -e "\033[1;35m--- Should have 54, 100 assertions, 0 failures. ---\033[0m"
+echo -e "\033[1;35m--- Should have 11 specs, 0 failures. ---\033[0m"
 echo ""
 
 echo "These should be examples of failing tests"
 command="${entry} spec/failure_egs.js spec/syntax_error.js"
 echo $command
 time $command #/nested/uber-nested
-echo -e "\033[1;35m--- Should have 6 tests, 6 assertions, 5 failures ---\033[0m"
+echo -e "\033[1;35m--- Should have 4 specs, 3 failures ---\033[0m"
 echo ""
